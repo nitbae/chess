f528af3119815f3776303a2366d8c2abfca8b748
nitbae <153553961+nitbae@users.noreply.github.com>
2023-12-16 22:50:36 +0300

    Checkpoint update vision.py
    - Implemented "separate" func with functional according to it's name
    - Revamped "fills" (now "blocks")
    - Update all of the Pawn  and some of Rook methods due to revamping "fills"
    - Implemented other Rook methods, finally finished this class

--- vision.py
+++ vision.py
@@ -1,55 +1,103 @@
 from process import SyntaxErr as SE
 from process import Basics as BS
-from board import board as brd
+from board import board, call
 
 class mth:
     @classmethod
     def plc(cls, pos, key):
-        exit("> SyntaxBreak -> Incorrect move") if SE.verify(pos, key="pos") else None
+        #exit("> SyntaxBreak -> Incorrect move") if SE.verify(pos, key="pos") else None
         return pos[0] if key == "col" else int(pos[1])
     
     @classmethod
-    def fills(cls, wl, key, rev=False, sp=False): #wl -> way list; sp -. special conditions
-        global brd
-        missM = []
-        missC = []
-        for m in wl:
-            match brd[8 - int(m[1])][ord(m[0]) - 65][0]:
-                case "w" if key == "move":
-                    wl[wl.index(m)] = "***"
-                    missM.append(m)
-                case "b" if key == "move" and sp == True:
-                    wl[wl.index(m)] = "***"
-                    missM.append(m)
-                case "b" if key == "cap":
-                    continue
-                case _ if key == "cap":
-                    wl[wl.index(m)] = "***"
-                    missC.append(m)
-        while "***" in wl:
-            wl.remove("***")
-        match rev:
-            case False:
-                return wl
-            case True if key == "move":
-                return missM
-            case True if key == "cap":
-                return missC
+    def blocks(cls, pos, waylist, key): #key -> 'move', 'capt'
+        global board
+        res = []
+        U, D, R, L = mth.separate(pos, waylist, [k for k in ["U", "D", "R", "L"]])
+        try:
+            U.reverse() if int(U[0][1]) > int(U[-1][1]) else None
+        except:
+            pass
+
+        try:
+            R.reverse() if ord(R[0][0]) > ord(R[-1][0]) else None
+        except:
+            pass
+
+        try:
+            D.reverse() if int(D[0][1]) < int(D[-1][1]) else None
+        except:
+            pass
+
+        try:
+            L.reverse() if ord(L[0][0]) < ord(L[-1][0]) else None
+        except:
+            pass
+        print(U, D, R, L)
+
+        for tr in U, D, R, L: #tr -- transition
+            flag = True
+            for m in tr:
+                match board[8 - int(m[1])][ord(m[0]) - 65][0]:
+                    case "w":
+                        if tr.index(m) == 0:
+                            pass
+                        else:
+                            res.extend(tr[:tr.index(m)]) if key == "move" else None
+                        flag = False
+                        break
+                    case "b":
+                        if tr.index(m) == 0:
+                            res.extend([tr[0]]) if key == "capt" else None
+                        else:
+                            res.extend(tr[:tr.index(m)]) if key == "move" else res.append(m)
+                        flag = False
+                        break
+            res.extend(tr) if flag and key == "move" else None
+            print(res)
+        return list(set(res))
+                
+    @classmethod
+    def separate(cls, pos, waylist, side): #side -> 'U', 'D', 'R', 'L'
+        U = []
+        D = []
+        R = []
+        L = []
+        p0 = mth.plc(pos, key='col')
+        p1 = str(mth.plc(pos, key='line'))
+
+        acco = {
+            "U": U,
+            "D": D,
+            "R": R,
+            "L": L
+        }
+
+        for m in waylist:
+            match m[0]:
+                case p0 if m[1] > p1:
+                    U.append(m)
+                case p0 if m[1] < p1:
+                    D.append(m)
+                case _:
+                    p0 = mth.plc(pos, key='col')
+                    if ord(m[0]) > ord(p0):
+                        R.append(m)
+                    else:
+                        L.append(m)
+        return [acco.get(k) for k in side]
 
 class Pawn:
     @classmethod
     def _way_(cls, pos):
-        res = [f"{mth.plc(pos, key='col')}{mth.plc(pos, key='line') + 1}"]
-        return res
+        return [f"{mth.plc(pos, key='col')}{mth.plc(pos, key='line') + 1}"]
     
     @classmethod
     def way(cls, pos):
-        return mth.fills(Pawn._way_(pos), key='move', sp=True)
+        return mth.blocks(pos, Pawn._way_(pos), key='move')
 
     @classmethod
     def _waycap_(cls, pos):
         res = [f"{chr(ord(mth.plc(pos, key='col')) + n)}{mth.plc(pos, key='line') + 1}" for n in [-1, 1]]
-        res.extend(["H9", "O6"])
         n = 0
         for i in BS.edges(res):
             res.pop(i - n)
@@ -58,7 +106,7 @@ class Pawn:
     
     @classmethod
     def waycap(cls, pos):
-        return mth.fills(Pawn._waycap_(pos), key='cap')
+        return mth.blocks(pos, Pawn._waycap_(pos), key='capt')
 
 class Rook:
     @classmethod
@@ -74,7 +122,7 @@ class Rook:
 
     @classmethod
     def way(cls, pos):
-        pass
+        return mth.blocks(pos, Rook._way_(pos), key='move')
 
     @classmethod
     def _waycap_(cls, pos):
@@ -82,4 +130,6 @@ class Rook:
 
     @classmethod
     def waycap(cls, pos):
-            pass
+        return mth.blocks(pos, Rook._waycap_(pos), key='capt')
+
+call(board)
